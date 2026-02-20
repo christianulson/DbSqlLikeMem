7f98af3d1a2d11b55d9804fd940800fd1f1c4a38
Christian Ulson <christian_ulson@hotmail.com>
2026-02-20 09:22:56 -0300

    Refatora execução LINQ para não depender de Dapper

--- src/DbSqlLikeMem.Oracle/OracleLinqProvider.cs
+++ src/DbSqlLikeMem.Oracle/OracleLinqProvider.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Linq.Expressions;
 using System.Reflection;
 
@@ -86,39 +85,10 @@ public sealed class OracleQueryProvider(
     public TResult Execute<TResult>(Expression expression)
     {
         ArgumentNullExceptionCompatible.ThrowIfNull(expression, nameof(expression));
-
-        // Traduz a árvore de expressão em SQL + parâmetros
         var translation = _translator.Translate(expression);
-
         var sql = translation.Sql ?? string.Empty;
-        var paramObj = translation.Params; // anonymous object / DynamicParameters / null
 
-        // IEnumerable (mas não string)
-        if (typeof(IEnumerable).IsAssignableFrom(typeof(TResult))
-            && typeof(TResult) != typeof(string))
-        {
-            var elementType = typeof(TResult).IsGenericType
-                ? typeof(TResult).GetGenericArguments().First()
-                : typeof(object);
-
-            var def = DapperLateBinding.FindSqlMapperMethodWithOptionalTail("Query", genericArgCount: 1);
-            var mi = def.MakeGenericMethod(elementType);
-
-            var invokeArgs = DapperLateBinding.BuildInvokeArgs(mi.GetParameters(), _cnn, sql, paramObj);
-            var data = mi.Invoke(null, invokeArgs)!;
-
-            return (TResult)data;
-        }
-        else
-        {
-            var def = DapperLateBinding.FindSqlMapperMethodWithOptionalTail("QuerySingleOrDefault", genericArgCount: 1);
-            var mi = def.MakeGenericMethod(typeof(TResult));
-
-            var invokeArgs = DapperLateBinding.BuildInvokeArgs(mi.GetParameters(), _cnn, sql, paramObj);
-            var data = mi.Invoke(null, invokeArgs);
-
-            return (TResult)data!;
-        }
+        return LinqQueryExecutor.Execute<TResult>(_cnn, sql, translation.Params);
     }
 
     // Implementação não-genérica, exigida pela interface
